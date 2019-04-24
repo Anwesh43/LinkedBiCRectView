e0f93d85e1f93f99cbb6e37f62034587e7c898ca
Anwesh43 <anweshthecool0@gmail.com>
2019-04-25 03:45:14 +0530

    adding static create method

--- bicrectview/src/main/java/com/anwesh/uiprojects/bicrectview/BiCRectView.kt
+++ bicrectview/src/main/java/com/anwesh/uiprojects/bicrectview/BiCRectView.kt
@@ -215,4 +215,13 @@ class BiCRectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : BiCRectView {
+            val view : BiCRectView = BiCRectView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
