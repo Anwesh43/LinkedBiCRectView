e5e885d3eba970fc1ed2b54d72c3416fccc8e8f9
Anwesh43 <anweshthecool0@gmail.com>
2019-04-25 03:46:30 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbicrectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbicrectview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbicrectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bicrectview.BiCRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiCRectView.create(this)
     }
 }
