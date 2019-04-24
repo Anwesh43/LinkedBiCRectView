3fa697d5b415e62c6a55df028b9190ed8c2b1c6c
Anwesh43 <anweshthecool0@gmail.com>
2019-04-25 03:31:12 +0530

    adding drawBCRNode canvas extension method

--- bicrectview/src/main/java/com/anwesh/uiprojects/bicrectview/BiCRectView.kt
+++ bicrectview/src/main/java/com/anwesh/uiprojects/bicrectview/BiCRectView.kt
@@ -9,6 +9,7 @@ import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
 import android.view.View
 import android.view.MotionEvent
 
@@ -20,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val foreColor : Int = Color.parseColor("#E64A19")
+val rFactor : Int = 5
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -27,3 +29,32 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawBiCLine(size : Float, scale : Float, paint : Paint) {
+    val x : Float = size * scale
+    save()
+    drawLine(0f, 0f, x, 0f, paint)
+    translate(x, 0f)
+    drawArc(RectF(-size / 5, -size / 5, size / 5, size / 5), 90f,
+            180f, false, paint)
+    restore()
+}
+
+fun Canvas.drawBCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND 
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    for (j in 0..(lines - 1)) {
+        drawBiCLine(size, sc1.divideScale(j, lines), paint)
+    }
+    restore()
+}
