ad6609839de67194de5c43c3b6bc6ebaee14ad71
Anwesh43 <anweshthecool0@gmail.com>
2019-04-25 03:41:37 +0530

    adding BiCRect class

--- bicrectview/src/main/java/com/anwesh/uiprojects/bicrectview/BiCRectView.kt
+++ bicrectview/src/main/java/com/anwesh/uiprojects/bicrectview/BiCRectView.kt
@@ -167,4 +167,28 @@ class BiCRectView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiCRect(var i : Int) {
+
+        private val root : BCRNode = BCRNode(0)
+        private var curr : BCRNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb  : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
